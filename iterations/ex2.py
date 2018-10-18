a3f4733b07a1b94edfa8b4839939da1cf7cbffe4
Killian <clota974@gmail.com>
2018-10-18 13:59:08 +0400

    18/10/2018

---
+++ iterations/ex2.py
@@ -0,0 +1,22 @@
+# KILLIAN CLOTAGATIDE / Iterations : Ex 2
+
+def U(x):
+    n = 0
+    Un = 3
+    while n<x:
+        n += 1
+        Un = 0.5*Un+5 # <==> U(n+1) = 0.5*U(n)+5
+    return Un
+
+p = float(input("Entrez la précision : "))
+
+N = -1 # Initialisation pour que N==0 lors de la première boucle
+f = -1
+while not(f>p): # On cherche à arrêter quand f>p
+    N += 1
+    f = abs(U(N)-10) #<==> |U(N)-10| 
+
+
+print(U(N))
+print(f)
+print("Le programme a retourné", N)
